a391c09721325c38fceb2634e0a2a173eeee165f
Nate <daviesnathanael45@gmail.com>
2025-12-18 15:25:09 +0000

    finished the coding stats area, progression bars now look apropriate on mobile and corrected.

--- script.js
+++ script.js
@@ -43,6 +43,8 @@ fetch("https://hackatime.hackclub.com/api/v1/users/22317/stats")
         const cpp = hackatime_stats.data.languages.find(l => l.name === "C++") ?? null;
         const c = hackatime_stats.data.languages.find(l => l.name === "C") ?? null;
         const python = hackatime_stats.data.languages.find(l => l.name === "Python") ?? null;
+        const html = hackatime_stats.data.languages.find(l => l.name === "HTML") ?? null;
+        const css = hackatime_stats.data.languages.find(l => l.name === "CSS") ?? null;
 
         //set total time 
         document.getElementById("code-time-text").textContent = hackatime_stats.data.human_readable_total;
@@ -51,13 +53,21 @@ fetch("https://hackatime.hackclub.com/api/v1/users/22317/stats")
         document.getElementById("c++-time-data").textContent = `C++: ${cpp.text}`;
         document.getElementById("cpp-bar").style.width = `${cpp.percent}%`;
 
-        //set the bar and time for cpp language
+        //set the bar and time for c language
         document.getElementById("c-time-data").textContent = `C: ${c.text}`;
         document.getElementById("c-bar").style.width = `${c.percent}%`;
 
         //set the bar and time for python language
         document.getElementById("python-time-data").textContent = `Python: ${python.text}`;
         document.getElementById("python-bar").style.width = `${python.percent}%`;
+
+        //set the bar and time for HTML language
+        document.getElementById("html-time-data").textContent = `HTML: ${html.text}`;
+        document.getElementById("html-bar").style.width = `${html.percent}%`;
+
+        //set the bar and time for python language
+        document.getElementById("css-time-data").textContent = `CSS: ${css.text}`;
+        document.getElementById("css-bar").style.width = `${css.percent}%`;
 
     });
 
