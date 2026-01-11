e6d7cd016a1768ebbb01219c899336ff88d9b9bf
Nate <daviesnathanael45@gmail.com>
2026-01-11 17:12:11 +0000

    added a web blog to my website, and updated timeline and new projects respectfully.

---
+++ scripts/blogscript.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () =>{
+    const perams = new URLSearchParams(window.location.search);
+    const file = perams.get("file");
+
+    if (!file) document.getElementById("body").textContent = "error: Text file was not found";
+
+    fetch(`blogs/${file}`)
+        .then(r => r.text())
+        .then(t => {
+            document.getElementById("body").innerHTML = t;
+        });
+});
