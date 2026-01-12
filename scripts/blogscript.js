163b4efafa215a146a785e56c639489810e703b4
Nate <daviesnathanael45@gmail.com>
2026-01-12 09:47:04 +0000

    second potential blog fix

--- scripts/blogscript.js
+++ scripts/blogscript.js
@@ -1,10 +1,21 @@
+const isLocal =
+    location.hostname === "localhost" ||
+    location.hostname === "127.0.0.1" ||
+    location.hostname === "";
+
 document.addEventListener("DOMContentLoaded", () =>{
     const perams = new URLSearchParams(window.location.search);
     const file = perams.get("file");
 
     if (!file) document.getElementById("body").textContent = "error: Text file was not found";
 
-    fetch(`blogs/${file}`, {cache: "no-store"})
+    if (isLocal){
+        let prefix = "";
+    }else{
+        let prefix = "https://me.natesite.co.uk"
+    }
+
+    fetch(`${prefix}/blogs/${file}`)
         .then(r => r.text())
         .then(t => {
             document.getElementById("body").innerHTML = t;
